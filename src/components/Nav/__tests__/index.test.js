96f34367295273847f5d2f8b4adcd53a2b1eceee
japankid-code <japankid.jake@gmail.com>
2021-07-03 11:02:19 -0500

    add tests, close #1 earlier woops

---
+++ src/components/Nav/__tests__/index.test.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Nav from "..";
+
+afterEach(cleanup);
+
+describe("Nav component", () => {
+  it("renders", () => {
+    render(<Nav />);
+  });
+  it("matches snapshot DOM node structure", () => {
+    const { asFragment } = render(<Nav />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+describe("emoji is visible", () => {
+  it("inserts emoji into the h2", () => {
+    const { getByLabelText } = render(<Nav />);
+    expect(getByLabelText("camera")).toHaveTextContent("📸");
+  });
+});
+
+describe("links are visible", () => {
+  it("inserts text into the links", () => {
+    // Arrange
+    const { getByTestId } = render(<Nav />);
+    // Assert
+    expect(getByTestId("link")).toHaveTextContent("Oh Snap!");
+    expect(getByTestId("about")).toHaveTextContent("About me");
+  });
+});
