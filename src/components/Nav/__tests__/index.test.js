b43c0b42c4978d8998aeb88767999ff38568500a
japankid-code <japankid.jake@gmail.com>
2021-07-05 11:11:22 -0500

    add gallery section, close #2

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -3,11 +3,23 @@ import { render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Nav from "..";
 
+const categories = [
+  { name: "portraits", description: "Portraits of people in my life" },
+];
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+
 afterEach(cleanup);
 
 describe("Nav component", () => {
   it("renders", () => {
-    render(<Nav />);
+    render(
+      <Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+      />
+    );
   });
   it("matches snapshot DOM node structure", () => {
     const { asFragment } = render(<Nav />);
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,11 +1,14 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
   const { categories = [], setCurrentCategory, currentCategory } = props;
-  useEffect(() => {
-    document.title = capitalizeFirstLetter(currentCategory.name);
-  }, [currentCategory]);
+
+  const handleClick = (item) => {
+    console.log(item);
+    return item;
+  };
+
   return (
     <header className="flex-row px-1">
       <h2>
@@ -24,14 +27,12 @@ function Nav(props) {
               About me
             </a>
           </li>
-          <li className="mx-2">
-            <span>Contact</span>
+          <li className={"mx-2"}>
+            <span onClick={() => handleClick("Contact")}>Contact</span>
           </li>
           {categories.map((category) => (
             <li
-              className={`mx-1 ${
-                currentCategory.name === category.name && "navActive"
-              }`}
+              className={`mx-1 ${currentCategory.name === category.name}`}
               key={category.name}
             >
               <span
